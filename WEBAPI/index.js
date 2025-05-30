5884dc742d70a4bb2c2d6a8cb725f4509ecec24e
Abdul Samad <itxsamad@icloud.com>
2025-05-30 20:05:18 +0000

    added more get functions

--- WEBAPI/index.js
+++ WEBAPI/index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express());
 
 
-app.get('/',async(req,res)=>{
+app.get('/',async(req,res)=>{ 
     try{
         res.json({Message:"AOA, WELCOME TO SMIU...."});
     }catch(err){
@@ -25,8 +25,43 @@ app.get('/teacher',async(req,res)=>{
     }
 });
 
+app.get('/teaches', async(req, res) => {
+    try {
+        const output = await pool.query('select * from teaches');
+        res.json(output.rows);
+    } catch(err) {
+        res.status(500).json({Error: err.message});
+    }
+});
 
-const PORT = process.env.PORT | 8090;
+app.get('/course', async(req, res) => {
+    try {
+        const output = await pool.query('select * from course');
+        res.json(output.rows);
+    } catch(err) {
+        res.status(500).json({Error: err.message});
+    }
+});
+
+app.get('/enrollment', async(req, res) => {
+    try {
+        const output = await pool.query('select * from enrollment');
+        res.json(output.rows);
+    } catch(err) {
+        res.status(500).json({Error: err.message});
+    }
+});
+
+app.get('/student', async(req, res) => {
+    try {
+        const output = await pool.query('select * from student');
+        res.json(output.rows);
+    } catch(err) {
+        res.status(500).json({Error: err.message});
+    }
+});
+
+const PORT = process.env.PORT || 8090;
 app.listen(PORT,()=>{
     console.log(`Connect Successfully... Running on Port ${PORT}`);
 });
